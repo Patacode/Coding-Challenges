2f79330d8b20b370f2202300606e59517d9a16ff
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-16 20:08:28 +0100

    test: add unit test for hasher
    add unit test for the 32-bit variant of #fnv1 method which
    should return the hash value of the provided string using
    the FNV-1 algorithm

---
+++ #53/spec/hasher_spec.rb
@@ -0,0 +1,9 @@
+require 'hasher'
+
+RSpec.describe Hasher do
+  describe '#fnv1' do
+    it "returns the provided string's hash using the 32-bit FNV-1 algorithm" do
+      expect(Hasher.fnv1('hello', variant: 32)).to eq(3069866343)
+    end
+  end
+end
