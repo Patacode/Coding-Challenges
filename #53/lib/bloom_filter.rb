4629b343a8186ad9fa9eccd3db2fdb8876bf42f7
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-16 17:24:57 +0100

    feat: add basic bloom filter impl.
    it only returns its required bit size for the moment. Nothing
    much

--- #53/lib/bloom_filter.rb
+++ #53/lib/bloom_filter.rb
@@ -1,1 +1,21 @@
+# frozen_string_literal: true
 
+class BloomFilter
+  attr_reader :size
+
+  def initialize(datasource, epsilon: 0.1)
+    @size = required_bit_qty(filesize(datasource), epsilon)
+  end
+
+  private
+
+  def filesize(filepath)
+    File.open(filepath) do |file|
+      file.each_line.count
+    end
+  end
+
+  def required_bit_qty(elem_qty, epsilon)
+    -(elem_qty * Math.log(epsilon) / Math.log(2)**2).floor
+  end
+end
