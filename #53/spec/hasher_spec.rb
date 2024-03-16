c5c29a9d9df9733fa774ec8d545319b1e827c1ab
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-16 20:54:28 +0100

    test: add unit test for hasher
    add unit test for 64-bit variant of #fnv1
    method which should return the hash value
    of the provided string using the 64-bit FNV-1
    algorithm

--- #53/spec/hasher_spec.rb
+++ #53/spec/hasher_spec.rb
@@ -7,5 +7,9 @@ RSpec.describe Hasher do
     it "returns the provided string's hash using the 32-bit FNV-1 algorithm" do
       expect(Hasher.fnv1('hello', variant: 32)).to eq(3_069_866_343)
     end
+
+    it "returns the provided string's hash using the 64-bit FNV-1 algorithm" do
+      expect(Hasher.fnv1('hello', variant: 64)).to eq(8_883_723_591_023_973_575)
+    end
   end
 end
