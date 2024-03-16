1f775c668dc309d3279d42b71d7d6b44241e1bf5
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-16 21:03:09 +0100

    test: add unit test for hasher
    add unit test for 32-bit variant of #fnv1 method with
    explicit offset basis

--- #53/spec/hasher_spec.rb
+++ #53/spec/hasher_spec.rb
@@ -11,5 +11,11 @@ RSpec.describe Hasher do
     it "returns the provided string's hash using the 64-bit FNV-1 algorithm" do
       expect(Hasher.fnv1('hello', variant: 64)).to eq(8_883_723_591_023_973_575)
     end
+
+    it "returns the correct hash when offset basis is provided explicitly" do
+      actual = Hasher.fnv1('hello', variant: 32, offset_basis: 3_069_866_343)
+
+      expect(actual).to eq(1_352_665_897)
+    end
   end
 end
