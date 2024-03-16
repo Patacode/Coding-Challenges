006e40d8214dfffe30bc44e790d489c176b148f5
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-16 16:59:27 +0100

    test: update unit tests for bloom filter
    start with unit tests in the order of the requirements. Thus
    first with the required bit size of the data structure (TDD).

--- #53/spec/bloom_filter_spec.rb
+++ #53/spec/bloom_filter_spec.rb
@@ -2,16 +2,12 @@ require 'bloom_filter'
 
 RSpec.describe BloomFilter do
   before(:context) do
-    @bloom_filter = BloomFilter.new('dict_sample.txt')
+    @bloom_filter = BloomFilter.new('dict_sample.txt', epsilon: 0.1)
   end
 
-  describe '#include?' do
-    it 'returns true if the provided value is probably in the set' do
-      expect(@bloom_filter.include?('coding')).to be(true)
-    end
-
-    it 'returns false if the provided value is probably not in the set' do
-      expect(@bloom_filter.include?('albito')).to be(false)
+  describe '#size' do
+    it 'returns the number of bits used by the bloom filter' do
+      expect(@bloom_filter.size).to eq(480)
     end
   end
 end
