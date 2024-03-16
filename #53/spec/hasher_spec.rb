deab9a465dfd7f9077dfba06957002dde7db9bd7
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-16 22:19:33 +0100

    style(hasher_spec): replace double-quotes by single-quotes where possible

--- #53/spec/hasher_spec.rb
+++ #53/spec/hasher_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Hasher do
       expect(Hasher.fnv1('hello', variant: 64)).to eq(8_883_723_591_023_973_575)
     end
 
-    it "returns the correct hash when offset basis is provided explicitly" do
+    it 'returns the correct hash when offset basis is provided explicitly' do
       actual = Hasher.fnv1('hello', variant: 32, offset_basis: 3_069_866_343)
 
       expect(actual).to eq(1_352_665_897)
