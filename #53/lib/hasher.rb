9426a223388e17935c37f808833ed9792677461d
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-16 21:07:25 +0100

    fix: update fnv1 method of hasher class
    add an offset_basis keyword argument to compute
    the hash value based on an explicit offset basis
    value

--- #53/lib/hasher.rb
+++ #53/lib/hasher.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class Hasher
-  def self.fnv1(string, variant: 64)
+  def self.fnv1(string, variant: 64, offset_basis: nil)
     prime, offset_basis, pack_template =
       case variant
-      when 32 then [16_777_619, 2_166_136_261, 'L']
-      when 64 then [1_099_511_628_211, 14_695_981_039_346_656_037, 'Q']
+      when 32 then [16_777_619, offset_basis || 2_166_136_261, 'L']
+      when 64
+        [1_099_511_628_211, offset_basis || 14_695_981_039_346_656_037, 'Q']
       else raise ArgumentError, "Variant #{variant}-bit not supported"
       end
     
