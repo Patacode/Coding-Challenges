62bb4727164cead9354642be0ba21a0f0e9a2ea3
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-16 22:16:47 +0100

    style(hasher): remove trailing whitespace

--- #53/lib/hasher.rb
+++ #53/lib/hasher.rb
@@ -9,7 +9,7 @@ class Hasher
         [1_099_511_628_211, offset_basis || 14_695_981_039_346_656_037, 'Q']
       else raise ArgumentError, "Variant #{variant}-bit not supported"
       end
-    
+
     string.each_byte.reduce(offset_basis) do |hash, byte|
       [hash * prime].pack(pack_template).unpack1(pack_template) ^ byte
     end
