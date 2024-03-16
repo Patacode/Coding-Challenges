93f1eeb5e28501ddf99cd73cf11558cf75cc74af
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-16 20:25:23 +0100

    style: add _ separator in integer literals defined in hasher

--- #53/lib/hasher.rb
+++ #53/lib/hasher.rb
@@ -4,8 +4,8 @@ class Hasher
   def self.fnv1(string, variant: 64)
     prime, offset_basis =
       case variant
-      when 32 then [16777619, 2166136261]
-      when 64 then [1099511628211, 14695981039346656037]
+      when 32 then [16_777_619, 2_166_136_261]
+      when 64 then [1_099_511_628_211, 14_695_981_039_346_656_037]
       else raise ArgumentError, "Variant #{variant}-bit not supported"
       end
 
