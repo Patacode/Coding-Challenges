3b9d4a4d669f4de1be7438b561b00e99f081d438
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-16 20:57:02 +0100

    fix: update fnv1 hasher method to support 64-bit variant

--- #53/lib/hasher.rb
+++ #53/lib/hasher.rb
@@ -2,15 +2,15 @@
 
 class Hasher
   def self.fnv1(string, variant: 64)
-    prime, offset_basis =
+    prime, offset_basis, pack_template =
       case variant
-      when 32 then [16_777_619, 2_166_136_261]
-      when 64 then [1_099_511_628_211, 14_695_981_039_346_656_037]
+      when 32 then [16_777_619, 2_166_136_261, 'L']
+      when 64 then [1_099_511_628_211, 14_695_981_039_346_656_037, 'Q']
       else raise ArgumentError, "Variant #{variant}-bit not supported"
       end
-
+    
     string.each_byte.reduce(offset_basis) do |hash, byte|
-      [hash * prime].pack('L').unpack1('L') ^ byte
+      [hash * prime].pack(pack_template).unpack1(pack_template) ^ byte
     end
   end
 end
