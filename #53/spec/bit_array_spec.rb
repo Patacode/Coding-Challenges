2ed16c3737c5631b318434ec0d82c79e56224e48
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-17 23:53:41 +0100

    test(bit_array): remove uncessary expectations

--- #53/spec/bit_array_spec.rb
+++ #53/spec/bit_array_spec.rb
@@ -4,32 +4,21 @@ require 'bit_array'
 
 RSpec.describe BitArray do
   describe '::new' do
-    it 'returns a new bit array instance of the desired size' do
-      expect { BitArray.new(10) }.not_to raise_error(ArgumentError)
+    it 'returns a new bit array of the desired size' do
+      BitArray.new(10)
     end
 
-    it 'returns a new bit array instance based on some given binary string' do
-      expect { BitArray.new([255, 10, 20].pack('C*')) }.not_to raise_error(
-        ArgumentError
-      )
+    it 'returns a new bit array based on some given binary string' do
+      BitArray.new([255, 10, 20].pack('C*'))
     end
 
-    it(
-      'returns a new bit array instance using some given element size in bits'
-    ) do
-      expect { BitArray.new(100, bits_per_item: 64) }.not_to raise_error(
-        ArgumentError
-      )
+    it 'returns a new bit array using some given element size in bits' do
+      BitArray.new(100, bits_per_item: 64)
     end
 
     it 'raises an ArgumentError if bits_per_item is < 1 or > 64' do
-      expect { BitArray.new(100, bits_per_item: 0) }.to raise_error(
-        ArgumentError
-      )
-
-      expect { BitArray.new(100, bits_per_item: 65) }.to raise_error(
-        ArgumentError
-      )
+      expect { BitArray.new(100, bits_per_item: 0) }.to raise_error(ArgumentError)
+      expect { BitArray.new(100, bits_per_item: 65) }.to raise_error(ArgumentError)
     end
   end
 end
