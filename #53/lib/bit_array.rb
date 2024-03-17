0a3e9f93951d9f33677a550566fcafc92cf24b1c
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-18 00:02:22 +0100

    feat(bit_array): add support to retrieve size accurately

--- #53/lib/bit_array.rb
+++ #53/lib/bit_array.rb
@@ -1,5 +1,10 @@
 class BitArray
+  attr_reader :size
+
   def initialize(initial_data, bits_per_item: 64)
     raise ArgumentError if bits_per_item < 1 || bits_per_item > 64
+
+    @size =
+      initial_data.is_a?(String) ? initial_data.bytesize * 8 : initial_data
   end
 end
