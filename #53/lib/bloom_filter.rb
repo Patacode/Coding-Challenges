a5303db053de02df64981699850d7c678ea42fa9
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-17 12:03:52 +0100

    fix(bloom_filter): adapt constructor to new unit tests

--- #53/lib/bloom_filter.rb
+++ #53/lib/bloom_filter.rb
@@ -5,11 +5,9 @@ require 'hasher'
 class BloomFilter
   attr_reader :size, :hash_function_count
 
-  def initialize(datasource, epsilon: 0.1)
-    line_qty = filesize(datasource)
-
-    @size = required_bit_qty(line_qty, epsilon)
-    @hash_function_count = optimal_hash_function_qty(@size, line_qty)
+  def initialize(element_count, epsilon: 0.1)
+    @size = required_bit_qty(element_count, epsilon)
+    @hash_function_count = optimal_hash_function_qty(@size, element_count)
     @bit_array = 0
   end
 
