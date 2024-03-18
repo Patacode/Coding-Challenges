aa20b0d40ea03d2da4536c87ce90af874b227f1a
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-18 01:17:08 +0100

    feat(bit_array): add support to retrieve internal array clone

--- #53/lib/bit_array.rb
+++ #53/lib/bit_array.rb
@@ -8,5 +8,29 @@ class BitArray
 
     @size =
       initial_data.is_a?(String) ? initial_data.bytesize * 8 : initial_data
+    @internal_array = init_internal_array(initial_data, @size, bits_per_item)
+  end
+
+  def internal_array_clone
+    @internal_array.clone
+  end
+
+  private
+
+  def init_internal_array(initial_data, size, bits_per_item)
+    res = [0] * (size / bits_per_item.to_f).ceil
+    return res unless initial_data.is_a?(String)
+
+    item_pointer = -1
+    bits_processed = 0
+    initial_data.each_byte do |byte|
+      item_pointer += 1 if (bits_processed % bits_per_item) == 0
+      bits_processed += 8
+
+      res[item_pointer] <<= 8
+      res[item_pointer] |= byte
+    end
+
+    res
   end
 end
