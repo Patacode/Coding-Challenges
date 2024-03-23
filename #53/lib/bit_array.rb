5fa3155940f12e1f904eec44be4a9fbe4ede8fe1
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-23 20:02:25 +0100

    feat(bit_array): add support for range access

--- #53/lib/bit_array.rb
+++ #53/lib/bit_array.rb
@@ -16,12 +16,47 @@ class BitArray
   end
 
   def [](index)
-    raise IndexError if index < 0 || index >= size
+    if index.is_a?(Range)
+      parsed_begin = index.begin || 0
+      parsed_end = index.end || @size - 1
+      lower_bound = parsed_begin
+      upper_bound = index.exclude_end? ? parsed_end - 1 : parsed_end
 
-    item_index = index / bits_per_item
-    offset = @internal_array[item_index].bit_length - index - 1
+      raise RangeError, "lower bound" if lower_bound < 0 || lower_bound >= @size
+      raise RangeError, "upper bound" if upper_bound < 0 || upper_bound >= @size
 
-    (@internal_array[item_index] >> offset) & 0x1
+      return [] if lower_bound > upper_bound
+
+      res = []
+      upper_size = @size < @bits_per_item ? @size : @bits_per_item
+      lower_pow = upper_size - lower_bound
+      upper_pow = upper_bound < upper_size ? upper_size - upper_bound - 1 : 0
+      mask = 2**lower_pow - 2**upper_pow
+      res << (@internal_array[0] & mask)
+
+      item_index = 1
+      items_count = (upper_bound / @bits_per_item) + 1
+
+      processed_bound = items_count - 1
+      while item_index < processed_bound
+        res << @internal_array[item_index]
+        item_index += 1
+      end
+
+      if items_count > 1
+        remaining_bits = @size - (@bits_per_item * (items_count - 1))
+        offset = remaining_bits - (upper_bound % remaining_bits) - 1
+        res << (@internal_array[-1] >> offset)
+      end
+
+      res
+    else
+      raise IndexError if index < 0 || index >= size
+      item_index = index / @bits_per_item
+      offset = @internal_array[item_index].bit_length - index - 1
+
+      (@internal_array[item_index] >> offset) & 0x1
+    end
   end
 
   def []=(index, value)
@@ -34,8 +69,8 @@ class BitArray
 
     return if bit.zero? 
 
-    item_index = index / bits_per_item
-    offset = bits_per_item - index
+    item_index = index / @bits_per_item
+    offset = @bits_per_item - (index % @bits_per_item)
 
     @internal_array[item_index] |= (2**offset >> 1)
   end
