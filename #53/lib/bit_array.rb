5bcef29ca1bb9239dec102280e9298feb973d468
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-24 18:44:17 +0100

    fix(bit_array): init data considered as array

--- #53/lib/bit_array.rb
+++ #53/lib/bit_array.rb
@@ -7,7 +7,7 @@ class BitArray
     raise ArgumentError if bits_per_item < 1 || bits_per_item > 64
     raise ArgumentError if bits_per_item % 8 != 0
 
-    @size = init_size(initial_data)
+    @size = init_size(initial_data, bits_per_item)
     @internal_array = init_internal_array(initial_data, @size, bits_per_item)
     @bits_per_item = bits_per_item
   end
@@ -106,25 +106,17 @@ class BitArray
 
   private
 
-  def init_size(initial_data)
-    initial_data.is_a?(String) ? initial_data.bytesize * 8 : initial_data
+  def init_size(initial_data, bits_per_item)
+    if initial_data.is_a?(Array)
+      bits_per_item * initial_data.length
+    else
+      initial_data
+    end
   end
 
   def init_internal_array(initial_data, size, bits_per_item)
     res = [0] * (size / bits_per_item.to_f).ceil
-    return res unless initial_data.is_a?(String)
-
-    item_pointer = -1
-    bits_processed = 0
-    initial_data.each_byte do |byte|
-      item_pointer += 1 if (bits_processed % bits_per_item).zero?
-      bits_processed += 8
-
-      res[item_pointer] <<= 8
-      res[item_pointer] |= byte
-    end
-
-    res
+    initial_data.is_a?(Array) ? initial_data.clone : res
   end
 
   alias at []
