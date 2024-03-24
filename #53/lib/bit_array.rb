7b6cd113bc5f2b4364235de08645758bb96c1e48
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-24 19:22:55 +0100

    fix(bit_array): remove bit range access support

--- #53/lib/bit_array.rb
+++ #53/lib/bit_array.rb
@@ -13,67 +13,18 @@ class BitArray
   end
 
   def [](index)
-    if index.is_a?(Range)
-      bounded_start = index.begin || 0
-      bounded_end = index.end || @size - 1
-      bounded_end = index.exclude_end? ? bounded_end - 1 : bounded_end
+    raise IndexError if index < 0 || index >= @size
 
-      raise RangeError if bounded_start < 0 || bounded_start >= @size
-      raise RangeError if bounded_end < 0 || bounded_end >= @size
-
-      res = []
-      item_index = bounded_start / @bits_per_item
-      processed_bits = bounded_start
-      left_over_bits = bounded_start % @bits_per_item
-      last_item_bit_size =
+    item_index = index / @bits_per_item
+    item_bit_size =
+      if item_index == @internal_array.length - 1
         @size - ((@internal_array.length - 1) * @bits_per_item)
-
-      while processed_bits <= bounded_end
-        item_bit_size =
-          if item_index == @internal_array.length - 1
-            last_item_bit_size
-          else
-            @bits_per_item
-          end
-        offset = item_bit_size - (bounded_start % @bits_per_item)
-        if item_index == @internal_array.length - 1
-          end_offset = item_bit_size - (bounded_end % @bits_per_item) - 1
-          res << (@internal_array[item_index] >> end_offset)
-        else
-          res << (@internal_array[item_index] & ((2**offset) - 1))
-        end
-        item_index += 1
-        processed_bits += offset + left_over_bits
-
-        if left_over_bits > 0 && item_index < @internal_array.length
-          item_bit_size =
-            if item_index == @internal_array.length - 1
-              last_item_bit_size
-            else
-              @bits_per_item
-            end
-          bits_to_add =
-            @internal_array[item_index] >> (item_bit_size - left_over_bits)
-          res[item_index - 1] <<= left_over_bits
-          res[item_index - 1] |= bits_to_add
-        end
+      else
+        @bits_per_item
       end
+    offset = item_bit_size - (index % @bits_per_item) - 1 
 
-      res
-    else
-      raise IndexError if index < 0 || index >= @size
-
-      item_index = index / @bits_per_item
-      item_bit_size =
-        if item_index == @internal_array.length - 1
-          @size - ((@internal_array.length - 1) * @bits_per_item)
-        else
-          @bits_per_item
-        end
-      offset = item_bit_size - (index % @bits_per_item) - 1 
-
-      (@internal_array[item_index] >> offset) & 0x1
-    end
+    (@internal_array[item_index] >> offset) & 0x1
   end
 
   def []=(index, value)
