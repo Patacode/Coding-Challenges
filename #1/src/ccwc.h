b1052fda8a9901c9add833ea10d5f47bb5aa91cb
Maximilien Ballesteros <maxou.info@gmail.com>
2024-11-10 21:00:26 +0100

    doc(ccwc): add doc comment in header file
    Added documentation for count_bytes function

--- #1/src/ccwc.h
+++ #1/src/ccwc.h
@@ -1,1 +1,11 @@
+/**
+ * Returns the number of bytes contained in the given string
+ * as per the current locale.
+ * 
+ * - Invalid characters are not counted.
+ * - Given string must be NULL-terminated.
+ * 
+ * @param str the string to be processed
+ * @return the number of bytes contained in the given string.
+ */
 int count_bytes(const char *const str);
