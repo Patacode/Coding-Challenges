72f107f0e29ee6cc9d529d1a382abeb538345be2
Maximilien Ballesteros <maxou.info@gmail.com>
2024-11-10 21:04:07 +0100

    feat(ccwc): implement byte for multi-line strings

--- #1/src/ccwc.c
+++ #1/src/ccwc.c
@@ -1,6 +1,13 @@
+#include <stddef.h>
+
 #include "ccwc.h"
 
 int count_bytes(const char *const str)
 {
-	return 12;
+	int idx = 0;
+  while(str[idx] != '\0') {
+    idx++;
+  }
+
+  return idx;
 }
--- #1/test/ccwc.spec.c
+++ #1/test/ccwc.spec.c
@@ -4,7 +4,12 @@
 void setUp(void) {}
 void tearDown(void) {}
 
-void test_byte_count(void)
+/**
+ * Test scenarios:
+ * - one line containing ASCII characters only
+ * - two lines containing ASCII characters only
+ */
+void test_byte_count_one_line(void)
 {
 	const char *const str = "Hello, World";
 	const int actual_byte_count = count_bytes(str);
@@ -13,9 +18,19 @@ void test_byte_count(void)
 	TEST_ASSERT_EQUAL_INT(expected_byte_count, actual_byte_count);
 }
 
+void test_byte_count_two_lines(void)
+{
+	const char *const str = "Hello, World\nMy friend !";
+	const int actual_byte_count = count_bytes(str);
+	const int expected_byte_count = 24;
+
+	TEST_ASSERT_EQUAL_INT(expected_byte_count, actual_byte_count);
+}
+
 int main(void)
 {
 	UNITY_BEGIN();
-	RUN_TEST(test_byte_count);
+	RUN_TEST(test_byte_count_one_line);
+	RUN_TEST(test_byte_count_two_lines);
 	return UNITY_END();
 }
